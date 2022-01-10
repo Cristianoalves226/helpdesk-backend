bf18e9836fe9937ff351594cdd5c7ef46cf167ca
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-01-10 12:47:47 -0400

    anotações jpa para classes de dominio

--- src/main/java/com/cristiano/helpdesk/domain/Tecnico.java
+++ src/main/java/com/cristiano/helpdesk/domain/Tecnico.java
@@ -1,5 +1,37 @@
 package com.cristiano.helpdesk.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Tecnico  extends Pessoa{
+
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<Chamado>();
+
+	public Tecnico() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
 
 }

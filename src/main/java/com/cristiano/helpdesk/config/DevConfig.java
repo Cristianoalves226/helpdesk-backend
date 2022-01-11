3cdf6007a56ad656ccde00dcee7231d711c30865
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-01-11 15:54:05 -0400

    Modificação no arquivo de configuração de desenvolvimento

--- src/main/java/com/cristiano/helpdesk/config/DevConfig.java
+++ src/main/java/com/cristiano/helpdesk/config/DevConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 import com.cristiano.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class DevConfig {
 	
 	@Autowired
